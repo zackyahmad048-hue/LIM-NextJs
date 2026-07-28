c2e55c5de46100de62990c12da3a7a4574594990
zackyahmad048-hue <zackyahmad048@gmail.com>
2026-07-28 10:13:16 +0700

    update: dashboard

--- app/(dashboard)/admin/page.tsx
+++ app/(dashboard)/admin/page.tsx
@@ -1,6 +1,8 @@
 import { PageContainer } from "@/components/admin/shared/page-container";
 import { DashboardClient } from "@/components/admin/dashboard/dashboard-client";
 
+export const dynamic = "force-dynamic";
+
 export default function AdminPage() {
   return (
     <PageContainer>
